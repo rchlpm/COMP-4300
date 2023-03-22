0776c25a0ec28e6aaddbb0edd325e6ce1b69d799
rpm00333 <111796142+rpm00333@users.noreply.github.com>
2023-03-21 22:38:43 -0400

    Update alu.vhd

--- alu.vhd
+++ alu.vhd
@@ -1,3 +1,4 @@
+library ieee;
 USE work.dlx_types.ALL;
 USE work.bv_arithmetic.ALL;
 
@@ -9,43 +10,50 @@ entity alu is
         operand2 : in dlx_word;
         operation : in alu_operation_code;
         result : out dlx_word;
-        error : out error_code
-    );
+        error : out error_code);
+
 end entity alu;
 
-architecture logic of alu is
+architecture unit of alu is
+ 
 begin
-    alu: process(operand1, operand2, operation) is
+    
+    alu: process(op1, op2, operation) is
+    
         variable overflow : boolean := false;
         variable operation_result : dlx_word;
-        variable divide_by_zero: boolean;
-    begin
+        variable div_by_zero: boolean;
+    
+begin
         error <= "0000";
         case(operation) is
-            when "0000" => -- unsigned add
-                bv_addu(operand1, operand2, operation_result, overflow);
+            when "0000" => 
+                bv_addu(op1, op2, operation_result, overflow);
                 if overflow then
                     error <= "0001" after prop_delay;
                 end if;
                 result <= operation_result after prop_delay;
-            when "0001" => -- unsigned subtract
+
+            when "0001" => 
                 bv_subu(operand1, operand2, operation_result, overflow);
                 if overflow then
                     error <= "0010" after prop_delay;
                 end if;
                 result <= operation_result after prop_delay;
-            when "0010" => -- two's complement add
-                bv_add(operand1, operand2, operation_result, overflow);
+
+            when "0010" => 
+                bv_add(op1, op2, operation_result, overflow);
                 if overflow then
-                    if operand1(31) = '0' AND operand2(31) = '0' then
+                    if op1(31) = '0' AND op2(31) = '0' then
                         error <= "0001" after prop_delay;
                     else
                         error <= "0010" after prop_delay;
                     end if;
                 end if;
                 result <= operation_result after prop_delay;
-            when "0011" => -- two's complement subtract
-                bv_sub(operand1, operand2, operation_result, overflow);
+                        
+            when "0011" => 
+                bv_sub(op1, op2, operation_result, overflow);
                 if overflow then
                     if operand1(31) = '0' then
                         error <= "0001" after prop_delay;
@@ -54,48 +62,57 @@ begin
                     end if;
                 end if;
                 result <= operation_result after prop_delay;
-            when "0100" => -- two's complement multiply
-                bv_mult(operand1, operand2, operation_result, overflow);
+                        
+            when "0100" => -- 
+                bv_mult(op1, op2, operation_result, overflow);
                 if overflow then
-                    if operand1(31) = operand2(31) then
+                    if op1(31) = op2(31) then
                         error <= "0001" after prop_delay;
                     else
                         error <= "0010" after prop_delay;
                     end if;
                 end if;
                 result <= operation_result after prop_delay;
-            when "0101" => -- two's complement divide
-                bv_div(operand1, operand2, operation_result, divide_by_zero, overflow);
-                if divide_by_zero then
+                        
+            when "0101" => 
+                bv_div(op1, op2, operation_result, div_by_zero, overflow);
+                if div_by_zero then
                     error <= "0011" after prop_delay;
                 end if;
                 result <= operation_result after prop_delay;
-            when "0110" => -- logical AND
-                if operand1 /= x"00000000" AND operand2 /= x"00000000" then
+                    
+            when "0110" => -- 
+                if op1 /= x"00000000" AND op2 /= x"00000000" then
                     result <= x"00000001" after prop_delay;
                 else
                     result <= x"00000000" after prop_delay;
+                    
                 end if;
-            when "0111" => -- bitwise AND
+                    
+            when "0111" => --  
                 result <= operand1 AND operand2 after prop_delay;
-            when "1000" => -- logical OR
-                if operand1 /= x"00000000" OR operand2 /= x"00000000" then
+                    
+            when "1000" => --  OR
+                if op1 /= x"00000000" OR op2 /= x"00000000" then
                     result <= x"00000001" after prop_delay;
                 else
                     result <= x"00000000" after prop_delay;
                 end if;
-            when "1001" => -- bitwise OR
-                result <= operand1 OR operand2 after prop_delay;
-            when "1010" => -- logical NOT of operand1
-                if operand1 /= x"00000000" then
+                    
+            when "1001" => --  
+                result <= op1 OR op2 after prop_delay;
+            when "1010" => --  
+                if op1 /= x"00000000" then
                     result <= x"00000001" after prop_delay;
                 else
                     result <= x"00000000" after prop_delay;
                 end if;
-            when "1011" => -- bitwise NOT of operand1
+                    
+            when "1011" => -- 
                 result <= NOT operand1 after prop_delay;
             when others =>
                 result <= x"00000000";
+                    
         end case;
     end process alu;
-end architecture logic;
+end architecture unit;
